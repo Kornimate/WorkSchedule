36ca07550609fef0226aa4b3def2592cd1269649
Kornidesz Máté <mko@rushfiles.com>
2025-02-27 16:03:44 +0100

    new dates for March 2025 added

--- react-app/src/models/DataContainer.js
+++ react-app/src/models/DataContainer.js
@@ -3,43 +3,60 @@ import { TimeModel, DateModel } from "./ModelClasses";
 const DataContainer = {
     2025: {
         1: [
-            new TimeModel(new DateModel(2025,1,3),8,16),
-            new TimeModel(new DateModel(2025,1,6),8,16),
-            new TimeModel(new DateModel(2025,1,7),8,12),
-            new TimeModel(new DateModel(2025,1,8),8,12),
-            new TimeModel(new DateModel(2025,1,13),8,12),
-            new TimeModel(new DateModel(2025,1,14),8,12),
-            new TimeModel(new DateModel(2025,1,16),8,16),
-            new TimeModel(new DateModel(2025,1,17),8,16),
-            new TimeModel(new DateModel(2025,1,20),8,12),
-            new TimeModel(new DateModel(2025,1,21),8,12),
-            new TimeModel(new DateModel(2025,1,22),14,18),
-            new TimeModel(new DateModel(2025,1,23),11,19),
-            new TimeModel(new DateModel(2025,1,24),8,16),
-            new TimeModel(new DateModel(2025,1,25),8,12)
+            new TimeModel(new DateModel(2025,1,3),8,16), //8
+            new TimeModel(new DateModel(2025,1,6),8,16), //8
+            new TimeModel(new DateModel(2025,1,7),8,12), //4
+            new TimeModel(new DateModel(2025,1,8),8,12), //4
+            new TimeModel(new DateModel(2025,1,13),8,12), //4
+            new TimeModel(new DateModel(2025,1,14),8,12), //4
+            new TimeModel(new DateModel(2025,1,16),8,16), //8
+            new TimeModel(new DateModel(2025,1,17),8,16), //8
+            new TimeModel(new DateModel(2025,1,20),8,12), //4
+            new TimeModel(new DateModel(2025,1,21),8,12), //4
+            new TimeModel(new DateModel(2025,1,22),14,18), //4
+            new TimeModel(new DateModel(2025,1,23),11,19), //8
+            new TimeModel(new DateModel(2025,1,24),8,16), //8
+            new TimeModel(new DateModel(2025,1,25),8,12) //4
         ],
         2: [
-            new TimeModel(new DateModel(2025,2,3),11,12),
-            new TimeModel(new DateModel(2025,2,4),13,17),
-            new TimeModel(new DateModel(2025,2,5),13,18),
-            new TimeModel(new DateModel(2025,2,6),9,10),
-            new TimeModel(new DateModel(2025,2,7),8,12),
-            new TimeModel(new DateModel(2025,2,10),8,13),
-            new TimeModel(new DateModel(2025,2,11),8,13),
-            new TimeModel(new DateModel(2025,2,12),13,18),
-            new TimeModel(new DateModel(2025,2,14),8,12),
-            new TimeModel(new DateModel(2025,2,17),8,13),
-            new TimeModel(new DateModel(2025,2,18),8,13),
-            new TimeModel(new DateModel(2025,2,19),13,18),
-            new TimeModel(new DateModel(2025,2,20),13,17),
-            new TimeModel(new DateModel(2025,2,21),8,12),
-            new TimeModel(new DateModel(2025,2,24),8,13),
-            new TimeModel(new DateModel(2025,2,25),8,13),
-            new TimeModel(new DateModel(2025,2,26),13,18),
-            new TimeModel(new DateModel(2025,2,27),13,17),
-            new TimeModel(new DateModel(2025,2,28),8,12),
+            new TimeModel(new DateModel(2025,2,3),11,12), //1
+            new TimeModel(new DateModel(2025,2,4),13,17), //4
+            new TimeModel(new DateModel(2025,2,5),13,18), //5
+            new TimeModel(new DateModel(2025,2,6),9,10), //1
+            new TimeModel(new DateModel(2025,2,7),8,12), //4
+            new TimeModel(new DateModel(2025,2,10),8,13), //5
+            new TimeModel(new DateModel(2025,2,11),8,13), //5
+            new TimeModel(new DateModel(2025,2,12),13,18), //5
+            new TimeModel(new DateModel(2025,2,14),8,12), //4
+            new TimeModel(new DateModel(2025,2,17),8,13), //5
+            new TimeModel(new DateModel(2025,2,18),8,13), //5
+            new TimeModel(new DateModel(2025,2,19),13,18), //5
+            new TimeModel(new DateModel(2025,2,20),13,17), //4
+            new TimeModel(new DateModel(2025,2,21),8,12), //4
+            new TimeModel(new DateModel(2025,2,24),8,13), //5
+            new TimeModel(new DateModel(2025,2,25),8,13), //5
+            new TimeModel(new DateModel(2025,2,26),13,18), //5
+            new TimeModel(new DateModel(2025,2,27),13,17), //4
+            new TimeModel(new DateModel(2025,2,28),8,12), //4
         ],
-        3: [],
+        3: [
+            new TimeModel(new DateModel(2025,3,5),9,17), //8
+            new TimeModel(new DateModel(2025,3,6),13,17), //4
+            new TimeModel(new DateModel(2025,3,7),8,16), //8
+            new TimeModel(new DateModel(2025,3,10),8,16), //8
+            new TimeModel(new DateModel(2025,3,11),8,12), //4
+            new TimeModel(new DateModel(2025,3,12),13,17), //4
+            new TimeModel(new DateModel(2025,3,14),8,12), //4
+            new TimeModel(new DateModel(2025,3,17),8,16), //8
+            new TimeModel(new DateModel(2025,3,18),8,12), //4
+            new TimeModel(new DateModel(2025,3,19),13,17), //4
+            new TimeModel(new DateModel(2025,3,20),13,17), //4
+            new TimeModel(new DateModel(2025,3,24),8,12), //4
+            new TimeModel(new DateModel(2025,3,25),8,12), //4
+            new TimeModel(new DateModel(2025,3,26),8,12), //4
+            new TimeModel(new DateModel(2025,3,27),8,12), //4
+            new TimeModel(new DateModel(2025,3,28),8,12), //4
+        ],
         4: [],
         5: [],
         6: [],
--- react-app/src/models/UpdateTime.js
+++ react-app/src/models/UpdateTime.js
@@ -1,1 +1,1 @@
-export const LastUpdateTime = "2025.02.11";
+export const LastUpdateTime = "2025.02.21";
