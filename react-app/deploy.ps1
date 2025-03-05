853b595e73bd64f908fc75972ff8d07305790bd4
Kornimate <kornimate@gmail.com>
2025-03-05 12:42:52 +0100

    updated deploy script

--- react-app/deploy.ps1
+++ react-app/deploy.ps1
@@ -1,5 +1,8 @@
 $date = (Get-Date).ToString('yyyy.MM.dd')
 $data = "export const LastUpdateTime = `"$date`";" | Set-Content -Path ./src/models/UpdateTime.js
+git add ./src/models/UpdateTime.js
+git commit -m "updated time"
+git push
 Write-Host "Update Time changed"
 Write-Host "Starting deploy process..."
-npm run deploy
+# npm run deploy
