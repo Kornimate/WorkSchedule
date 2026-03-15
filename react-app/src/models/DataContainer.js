6dabfea159181d30884aa1bc045bfe28bfe4f3f3
kornimate <dev.profile.km@gmail.com>
2026-03-15 13:16:51 +0100

    indentation

--- react-app/src/models/DataContainer.js
+++ react-app/src/models/DataContainer.js
@@ -2,170 +2,170 @@ import { TimeModel, DateModel } from "./ModelClasses";
 import { OfflineWorkingState } from "./StateClasses";
 
 const DataContainer = {
-    2025: {
-        1: [
-            new TimeModel(new DateModel(2025,1,3),8,16), //8
-            new TimeModel(new DateModel(2025,1,6),8,16), //8
-            new TimeModel(new DateModel(2025,1,7),8,12), //4
-            new TimeModel(new DateModel(2025,1,8),8,12), //4
-            new TimeModel(new DateModel(2025,1,13),8,12), //4
-            new TimeModel(new DateModel(2025,1,14),8,12), //4
-            new TimeModel(new DateModel(2025,1,16),8,16), //8
-            new TimeModel(new DateModel(2025,1,17),8,16), //8
-            new TimeModel(new DateModel(2025,1,20),8,12), //4
-            new TimeModel(new DateModel(2025,1,21),8,12), //4
-            new TimeModel(new DateModel(2025,1,22),14,18), //4
-            new TimeModel(new DateModel(2025,1,23),11,19), //8
-            new TimeModel(new DateModel(2025,1,24),8,16), //8
-            new TimeModel(new DateModel(2025,1,25),8,12) //4
-        ],
-        2: [
-            new TimeModel(new DateModel(2025,2,3),11,12), //1
-            new TimeModel(new DateModel(2025,2,4),13,17), //4
-            new TimeModel(new DateModel(2025,2,5),13,18), //5
-            new TimeModel(new DateModel(2025,2,6),9,10), //1
-            new TimeModel(new DateModel(2025,2,7),8,12), //4
-            new TimeModel(new DateModel(2025,2,10),8,13), //5
-            new TimeModel(new DateModel(2025,2,11),8,13), //5
-            new TimeModel(new DateModel(2025,2,12),13,18), //5
-            new TimeModel(new DateModel(2025,2,14),8,12), //4
-            new TimeModel(new DateModel(2025,2,17),8,13), //5
-            new TimeModel(new DateModel(2025,2,18),8,13), //5
-            new TimeModel(new DateModel(2025,2,19),13,18), //5
-            new TimeModel(new DateModel(2025,2,20),13,17), //4
-            new TimeModel(new DateModel(2025,2,21),8,12), //4
-            new TimeModel(new DateModel(2025,2,24),8,13), //5
-            new TimeModel(new DateModel(2025,2,25),8,13), //5
-            new TimeModel(new DateModel(2025,2,26),13,18), //5
-            new TimeModel(new DateModel(2025,2,27),13,17), //4
-            new TimeModel(new DateModel(2025,2,28),8,12), //4
-        ],
-        3: [
-            new TimeModel(new DateModel(2025,3,5),13,17), //4
-            new TimeModel(new DateModel(2025,3,6),13,17), //4
-            new TimeModel(new DateModel(2025,3,7),8,16), //8
-            new TimeModel(new DateModel(2025,3,10),10,16), //6
-            new TimeModel(new DateModel(2025,3,11),8,12), //4
-            new TimeModel(new DateModel(2025,3,12),13,17), //4
-            new TimeModel(new DateModel(2025,3,13),13,17), //4
-            new TimeModel(new DateModel(2025,3,14),8,14), //6
-            new TimeModel(new DateModel(2025,3,17),8,16), //8
-            new TimeModel(new DateModel(2025,3,18),10,14), //4
-            new TimeModel(new DateModel(2025,3,19),13,17), //4
-            new TimeModel(new DateModel(2025,3,20),13,17), //4
-            new TimeModel(new DateModel(2025,3,24),8,12), //4
-            new TimeModel(new DateModel(2025,3,26),8,13), //5
-            new TimeModel(new DateModel(2025,3,27),8,13), //5
-            new TimeModel(new DateModel(2025,3,28),10,16), //6
-        ],
-        4: [
-            new TimeModel(new DateModel(2025,4,2),13,17), //4
-            new TimeModel(new DateModel(2025,4,3),9,13), //4
-            new TimeModel(new DateModel(2025,4,4),13,17), //4
-            new TimeModel(new DateModel(2025,4,8),9,13), //4
-            new TimeModel(new DateModel(2025,4,9),13,17), //4
-            new TimeModel(new DateModel(2025,4,10),8,12), //4
-            new TimeModel(new DateModel(2025,4,14),8,14), //6
-            new TimeModel(new DateModel(2025,4,15),9,14), //5
-            new TimeModel(new DateModel(2025,4,16),9,14), //5
-            new TimeModel(new DateModel(2025,4,18),8,14), //6
-            new TimeModel(new DateModel(2025,4,21),8,13), //5
-            new TimeModel(new DateModel(2025,4,22),9,13), //4
-            new TimeModel(new DateModel(2025,4,23),13,17), //4
-            new TimeModel(new DateModel(2025,4,24),9,13), //4
-            new TimeModel(new DateModel(2025,4,25),8,12), //4
-            new TimeModel(new DateModel(2025,4,28),8,13), //5
-            new TimeModel(new DateModel(2025,4,29),9,13), //4
-            new TimeModel(new DateModel(2025,4,30),13,17), //4
-        ],
-        5: [
-            new TimeModel(new DateModel(2025,5,1),8,12), //4
-            new TimeModel(new DateModel(2025,5,2),8,12), //4
-            new TimeModel(new DateModel(2025,5,5),8,12), //4
-            new TimeModel(new DateModel(2025,5,6),9,13), //4
-            new TimeModel(new DateModel(2025,5,7),13,17), //4
-            new TimeModel(new DateModel(2025,5,8),8,12), //4
-            new TimeModel(new DateModel(2025,5,9),8,12), //4
-            new TimeModel(new DateModel(2025,5,12),8,12), //4
-            new TimeModel(new DateModel(2025,5,14),13,17), //4
-            new TimeModel(new DateModel(2025,5,15),8,12), //4
-            new TimeModel(new DateModel(2025,5,16),8,12), //4
-            new TimeModel(new DateModel(2025,5,19),8,14), //6
-            new TimeModel(new DateModel(2025,5,20),9,15), //6
-            new TimeModel(new DateModel(2025,5,21),10,14), //4
-            new TimeModel(new DateModel(2025,5,22),8,12), //4
-            new TimeModel(new DateModel(2025,5,23),8,12), //4
-            new TimeModel(new DateModel(2025,5,26),8,12), //4
-            new TimeModel(new DateModel(2025,5,27),8,12), //4
-            new TimeModel(new DateModel(2025,5,28),10,14), //4
-        ],
-        6: [
-            new TimeModel(new DateModel(2025,6,4),8,16), //8
-            new TimeModel(new DateModel(2025,6,5),8,14), //6
-            new TimeModel(new DateModel(2025,6,6),8,14), //6
-            new TimeModel(new DateModel(2025,6,9),8,12), //4
-            new TimeModel(new DateModel(2025,6,12),8,12), //4
-            new TimeModel(new DateModel(2025,6,13),10,14).setState(new OfflineWorkingState()), //4
-            new TimeModel(new DateModel(2025,6,16),8,14), //6
-            new TimeModel(new DateModel(2025,6,17),8,12), //4
-            new TimeModel(new DateModel(2025,6,18),8,12), //4
-            new TimeModel(new DateModel(2025,6,19),8,12), //4
-            new TimeModel(new DateModel(2025,6,20),8,14), //6
-            new TimeModel(new DateModel(2025,6,23),8,16), //8
-            new TimeModel(new DateModel(2025,6,24),8,14), //6
-            new TimeModel(new DateModel(2025,6,25),8,14), //6
-            new TimeModel(new DateModel(2025,6,26),8,12), //4
-
-        ],
-        7: [],
-        8: [],
-        9: [
-            new TimeModel(new DateModel(2025,9,5),12,16), //4
-            new TimeModel(new DateModel(2025,9,7),12,16), //4
-            new TimeModel(new DateModel(2025,9,9),13,17), //4
-            new TimeModel(new DateModel(2025,9,10),12,16), //4
-            new TimeModel(new DateModel(2025,9,18),8,16), //8
-            new TimeModel(new DateModel(2025,9,19),12,16), //4
-            new TimeModel(new DateModel(2025,9,20),12,16), //4
-            new TimeModel(new DateModel(2025,9,23),13,17), //4
-            new TimeModel(new DateModel(2025,9,24),12,16), //4
-            new TimeModel(new DateModel(2025,9,25),8,16), //8
-            new TimeModel(new DateModel(2025,9,26),12,16), //4
-            new TimeModel(new DateModel(2025,9,27),12,16), //4
-            new TimeModel(new DateModel(2025,9,30),13,17), //4
-
-        ],
-        10: [],
-        11: [],
-        12: []
-    },
-    2026: {
-            1: [],
-            2: [],
-            3: [    
-                new TimeModel(new DateModel(2026,3,2),8,16), //8
-                new TimeModel(new DateModel(2026,3,3),8,11), //3
-                new TimeModel(new DateModel(2026,3,10),8,13), //5
-                new TimeModel(new DateModel(2026,3,11),8,16), //8
-                new TimeModel(new DateModel(2026,3,16),8,16), //8
-                new TimeModel(new DateModel(2026,3,17),8,13), //5
-                new TimeModel(new DateModel(2026,3,19),14,17), //3 ---> 40
-                new TimeModel(new DateModel(2026,3,24),8,13), //5
-                new TimeModel(new DateModel(2026,3,25),13,16), //3
-                new TimeModel(new DateModel(2026,3,27),8,10), //2
-                new TimeModel(new DateModel(2026,3,30),8,16), //8
-                new TimeModel(new DateModel(2026,3,31),8,10), //2
-            ],
-            4: [],
-            5: [],
-            6: [],
-            7: [],
-            8: [],
-            9: [],
-            10: [],
-            11: [],
-            12: []
-     }
-}
+  2025: {
+    1: [
+      new TimeModel(new DateModel(2025, 1, 3), 8, 16), //8
+      new TimeModel(new DateModel(2025, 1, 6), 8, 16), //8
+      new TimeModel(new DateModel(2025, 1, 7), 8, 12), //4
+      new TimeModel(new DateModel(2025, 1, 8), 8, 12), //4
+      new TimeModel(new DateModel(2025, 1, 13), 8, 12), //4
+      new TimeModel(new DateModel(2025, 1, 14), 8, 12), //4
+      new TimeModel(new DateModel(2025, 1, 16), 8, 16), //8
+      new TimeModel(new DateModel(2025, 1, 17), 8, 16), //8
+      new TimeModel(new DateModel(2025, 1, 20), 8, 12), //4
+      new TimeModel(new DateModel(2025, 1, 21), 8, 12), //4
+      new TimeModel(new DateModel(2025, 1, 22), 14, 18), //4
+      new TimeModel(new DateModel(2025, 1, 23), 11, 19), //8
+      new TimeModel(new DateModel(2025, 1, 24), 8, 16), //8
+      new TimeModel(new DateModel(2025, 1, 25), 8, 12), //4
+    ],
+    2: [
+      new TimeModel(new DateModel(2025, 2, 3), 11, 12), //1
+      new TimeModel(new DateModel(2025, 2, 4), 13, 17), //4
+      new TimeModel(new DateModel(2025, 2, 5), 13, 18), //5
+      new TimeModel(new DateModel(2025, 2, 6), 9, 10), //1
+      new TimeModel(new DateModel(2025, 2, 7), 8, 12), //4
+      new TimeModel(new DateModel(2025, 2, 10), 8, 13), //5
+      new TimeModel(new DateModel(2025, 2, 11), 8, 13), //5
+      new TimeModel(new DateModel(2025, 2, 12), 13, 18), //5
+      new TimeModel(new DateModel(2025, 2, 14), 8, 12), //4
+      new TimeModel(new DateModel(2025, 2, 17), 8, 13), //5
+      new TimeModel(new DateModel(2025, 2, 18), 8, 13), //5
+      new TimeModel(new DateModel(2025, 2, 19), 13, 18), //5
+      new TimeModel(new DateModel(2025, 2, 20), 13, 17), //4
+      new TimeModel(new DateModel(2025, 2, 21), 8, 12), //4
+      new TimeModel(new DateModel(2025, 2, 24), 8, 13), //5
+      new TimeModel(new DateModel(2025, 2, 25), 8, 13), //5
+      new TimeModel(new DateModel(2025, 2, 26), 13, 18), //5
+      new TimeModel(new DateModel(2025, 2, 27), 13, 17), //4
+      new TimeModel(new DateModel(2025, 2, 28), 8, 12), //4
+    ],
+    3: [
+      new TimeModel(new DateModel(2025, 3, 5), 13, 17), //4
+      new TimeModel(new DateModel(2025, 3, 6), 13, 17), //4
+      new TimeModel(new DateModel(2025, 3, 7), 8, 16), //8
+      new TimeModel(new DateModel(2025, 3, 10), 10, 16), //6
+      new TimeModel(new DateModel(2025, 3, 11), 8, 12), //4
+      new TimeModel(new DateModel(2025, 3, 12), 13, 17), //4
+      new TimeModel(new DateModel(2025, 3, 13), 13, 17), //4
+      new TimeModel(new DateModel(2025, 3, 14), 8, 14), //6
+      new TimeModel(new DateModel(2025, 3, 17), 8, 16), //8
+      new TimeModel(new DateModel(2025, 3, 18), 10, 14), //4
+      new TimeModel(new DateModel(2025, 3, 19), 13, 17), //4
+      new TimeModel(new DateModel(2025, 3, 20), 13, 17), //4
+      new TimeModel(new DateModel(2025, 3, 24), 8, 12), //4
+      new TimeModel(new DateModel(2025, 3, 26), 8, 13), //5
+      new TimeModel(new DateModel(2025, 3, 27), 8, 13), //5
+      new TimeModel(new DateModel(2025, 3, 28), 10, 16), //6
+    ],
+    4: [
+      new TimeModel(new DateModel(2025, 4, 2), 13, 17), //4
+      new TimeModel(new DateModel(2025, 4, 3), 9, 13), //4
+      new TimeModel(new DateModel(2025, 4, 4), 13, 17), //4
+      new TimeModel(new DateModel(2025, 4, 8), 9, 13), //4
+      new TimeModel(new DateModel(2025, 4, 9), 13, 17), //4
+      new TimeModel(new DateModel(2025, 4, 10), 8, 12), //4
+      new TimeModel(new DateModel(2025, 4, 14), 8, 14), //6
+      new TimeModel(new DateModel(2025, 4, 15), 9, 14), //5
+      new TimeModel(new DateModel(2025, 4, 16), 9, 14), //5
+      new TimeModel(new DateModel(2025, 4, 18), 8, 14), //6
+      new TimeModel(new DateModel(2025, 4, 21), 8, 13), //5
+      new TimeModel(new DateModel(2025, 4, 22), 9, 13), //4
+      new TimeModel(new DateModel(2025, 4, 23), 13, 17), //4
+      new TimeModel(new DateModel(2025, 4, 24), 9, 13), //4
+      new TimeModel(new DateModel(2025, 4, 25), 8, 12), //4
+      new TimeModel(new DateModel(2025, 4, 28), 8, 13), //5
+      new TimeModel(new DateModel(2025, 4, 29), 9, 13), //4
+      new TimeModel(new DateModel(2025, 4, 30), 13, 17), //4
+    ],
+    5: [
+      new TimeModel(new DateModel(2025, 5, 1), 8, 12), //4
+      new TimeModel(new DateModel(2025, 5, 2), 8, 12), //4
+      new TimeModel(new DateModel(2025, 5, 5), 8, 12), //4
+      new TimeModel(new DateModel(2025, 5, 6), 9, 13), //4
+      new TimeModel(new DateModel(2025, 5, 7), 13, 17), //4
+      new TimeModel(new DateModel(2025, 5, 8), 8, 12), //4
+      new TimeModel(new DateModel(2025, 5, 9), 8, 12), //4
+      new TimeModel(new DateModel(2025, 5, 12), 8, 12), //4
+      new TimeModel(new DateModel(2025, 5, 14), 13, 17), //4
+      new TimeModel(new DateModel(2025, 5, 15), 8, 12), //4
+      new TimeModel(new DateModel(2025, 5, 16), 8, 12), //4
+      new TimeModel(new DateModel(2025, 5, 19), 8, 14), //6
+      new TimeModel(new DateModel(2025, 5, 20), 9, 15), //6
+      new TimeModel(new DateModel(2025, 5, 21), 10, 14), //4
+      new TimeModel(new DateModel(2025, 5, 22), 8, 12), //4
+      new TimeModel(new DateModel(2025, 5, 23), 8, 12), //4
+      new TimeModel(new DateModel(2025, 5, 26), 8, 12), //4
+      new TimeModel(new DateModel(2025, 5, 27), 8, 12), //4
+      new TimeModel(new DateModel(2025, 5, 28), 10, 14), //4
+    ],
+    6: [
+      new TimeModel(new DateModel(2025, 6, 4), 8, 16), //8
+      new TimeModel(new DateModel(2025, 6, 5), 8, 14), //6
+      new TimeModel(new DateModel(2025, 6, 6), 8, 14), //6
+      new TimeModel(new DateModel(2025, 6, 9), 8, 12), //4
+      new TimeModel(new DateModel(2025, 6, 12), 8, 12), //4
+      new TimeModel(new DateModel(2025, 6, 13), 10, 14).setState(
+        new OfflineWorkingState(),
+      ), //4
+      new TimeModel(new DateModel(2025, 6, 16), 8, 14), //6
+      new TimeModel(new DateModel(2025, 6, 17), 8, 12), //4
+      new TimeModel(new DateModel(2025, 6, 18), 8, 12), //4
+      new TimeModel(new DateModel(2025, 6, 19), 8, 12), //4
+      new TimeModel(new DateModel(2025, 6, 20), 8, 14), //6
+      new TimeModel(new DateModel(2025, 6, 23), 8, 16), //8
+      new TimeModel(new DateModel(2025, 6, 24), 8, 14), //6
+      new TimeModel(new DateModel(2025, 6, 25), 8, 14), //6
+      new TimeModel(new DateModel(2025, 6, 26), 8, 12), //4
+    ],
+    7: [],
+    8: [],
+    9: [
+      new TimeModel(new DateModel(2025, 9, 5), 12, 16), //4
+      new TimeModel(new DateModel(2025, 9, 7), 12, 16), //4
+      new TimeModel(new DateModel(2025, 9, 9), 13, 17), //4
+      new TimeModel(new DateModel(2025, 9, 10), 12, 16), //4
+      new TimeModel(new DateModel(2025, 9, 18), 8, 16), //8
+      new TimeModel(new DateModel(2025, 9, 19), 12, 16), //4
+      new TimeModel(new DateModel(2025, 9, 20), 12, 16), //4
+      new TimeModel(new DateModel(2025, 9, 23), 13, 17), //4
+      new TimeModel(new DateModel(2025, 9, 24), 12, 16), //4
+      new TimeModel(new DateModel(2025, 9, 25), 8, 16), //8
+      new TimeModel(new DateModel(2025, 9, 26), 12, 16), //4
+      new TimeModel(new DateModel(2025, 9, 27), 12, 16), //4
+      new TimeModel(new DateModel(2025, 9, 30), 13, 17), //4
+    ],
+    10: [],
+    11: [],
+    12: [],
+  },
+  2026: {
+    1: [],
+    2: [],
+    3: [
+      new TimeModel(new DateModel(2026, 3, 2), 8, 16), //8
+      new TimeModel(new DateModel(2026, 3, 3), 8, 11), //3
+      new TimeModel(new DateModel(2026, 3, 10), 8, 13), //5
+      new TimeModel(new DateModel(2026, 3, 11), 8, 16), //8
+      new TimeModel(new DateModel(2026, 3, 16), 8, 16), //8
+      new TimeModel(new DateModel(2026, 3, 17), 8, 13), //5
+      new TimeModel(new DateModel(2026, 3, 19), 14, 17), //3 ---> 40
+      new TimeModel(new DateModel(2026, 3, 24), 8, 13), //5
+      new TimeModel(new DateModel(2026, 3, 25), 13, 16), //3
+      new TimeModel(new DateModel(2026, 3, 27), 8, 10), //2
+      new TimeModel(new DateModel(2026, 3, 30), 8, 16), //8
+      new TimeModel(new DateModel(2026, 3, 31), 8, 10), //2
+    ],
+    4: [],
+    5: [],
+    6: [],
+    7: [],
+    8: [],
+    9: [],
+    10: [],
+    11: [],
+    12: [],
+  },
+};
 
 export default DataContainer;
