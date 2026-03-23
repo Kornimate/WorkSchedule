74c2efc2ef53e082ad7d29c31cc5e0c86b96e26f
Kornidesz Máté <dev.profile.km@gmail.com>
2026-03-23 13:48:08 +0100

    updated schedule

--- react-app/src/models/DataContainer.js
+++ react-app/src/models/DataContainer.js
@@ -150,7 +150,7 @@ const DataContainer = {
       new TimeModel(new DateModel(2026, 3, 16), 8, 16), //8
       new TimeModel(new DateModel(2026, 3, 17), 8, 13), //5
       new TimeModel(new DateModel(2026, 3, 19), 14, 17), //3 ---> 40
-      new TimeModel(new DateModel(2026, 3, 24), 8, 13), //5
+      new TimeModel(new DateModel(2026, 3, 24), 14, 19), //5
       new TimeModel(new DateModel(2026, 3, 25), 13, 16), //3
       new TimeModel(new DateModel(2026, 3, 27), 8, 10), //2
       new TimeModel(new DateModel(2026, 3, 30), 8, 16), //8
