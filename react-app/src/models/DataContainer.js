904cb5765fb144a1c03ff95eed6c1a698537fc90
Kornidesz Máté <dev.profile.km@gmail.com>
2026-06-17 10:33:06 +0200

    updated schedule

--- react-app/src/models/DataContainer.js
+++ react-app/src/models/DataContainer.js
@@ -189,12 +189,11 @@ const DataContainer = {
     6: [
       new TimeModel(new DateModel(2026, 6, 5), 8, 14), //6
       new TimeModel(new DateModel(2026, 6, 10), 8, 12), //4
-      new TimeModel(new DateModel(2026, 6, 12), 8, 13), //5
       new TimeModel(new DateModel(2026, 6, 16), 8, 13), //5
-      new TimeModel(new DateModel(2026, 6, 17), 8, 13), //5
-      new TimeModel(new DateModel(2026, 6, 18), 8, 14), //6
-      new TimeModel(new DateModel(2026, 6, 19), 8, 14), //6 --> 56
-      new TimeModel(new DateModel(2026, 6, 24), 8, 13), //5 --v
+      new TimeModel(new DateModel(2026, 6, 17), 8, 15), //7
+      new TimeModel(new DateModel(2026, 6, 18), 8, 16), //8
+      new TimeModel(new DateModel(2026, 6, 19), 8, 15), //7 --> 56
+      new TimeModel(new DateModel(2026, 6, 24), 8, 14), //5 -->
       new TimeModel(new DateModel(2026, 6, 25), 8, 11), //3 --> extra already paid 8 hours
     ],
     7: [],
